3da6b2ed44968eaf314a7fb5cc135d69080f4583
ahmad <ahmadzafarani2013@gmail.com>
2020-07-31 00:38:25 +0430

    better libraries

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ import socket
 HOST = '127.0.0.1'
 PORT = 8000
 
-message = b'DELETE /test.txt HTTP/1.1\r\n\r\n'
+message = b'GET /test.txt HTTP/1.1\r\n\r\n'
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
