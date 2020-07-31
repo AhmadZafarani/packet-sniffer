e2d44dc83a0f2f0b0b3b9c9971d344913bfc91bd
ahmad <ahmadzafarani2013@gmail.com>
2020-07-31 12:07:08 +0430

    client-server program finished

--- client.py
+++ client.py
@@ -4,11 +4,10 @@ import socket
 
 HOST = '127.0.0.1'
 PORT = 8000
-
-message = b'GET /test.txt HTTP/1.1\r\n\r\n'
+message = b'POST /test HTTP/1.1\r\nHost: foo.example\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: 27\r\n\r\nfield1=value1&field2=value2\r\n\r\n'
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(message)
-    data = s.recv(512)
+    data = s.recv(1024)
     print(data.decode('ascii'))
--- server.py
+++ server.py
@@ -1,11 +1,36 @@
 # # YA HOSSEIN
 import http.server
 import socketserver
+from io import BytesIO
+
+
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+
+    def __init__(self, *args, directory=None, **kwargs):
+        super().__init__(*args, directory=directory, **kwargs)
+    
+
+    def do_POST(self):
+        try:
+            content_length = int(self.headers['Content-Length'])
+        except TypeError:
+            self.send_response(400)
+            return
+        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(b'This is POST request. ')
+        response.write(b'Received: ')
+        response.write(body)
+        self.wfile.write(response.getvalue())
+
+
 
 PORT = 8000
+HOST = "127.0.0.1"
+Handler = MyHandler
 
-Handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("127.0.0.1", PORT), Handler) as httpd:
+with socketserver.TCPServer((HOST, PORT), Handler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
